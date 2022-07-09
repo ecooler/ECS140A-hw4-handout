f11e07828b1132610a627f7393e025bc5a0a568d
powcoder代写 <powcoder@163.com>
2022-07-09 10:49:28 +0800

    程序代写 加微信 powcoder

---
+++ puzzle/puzzle.pl
@@ -0,0 +1,3 @@
+solve(F1, W1, G1, C1, F2, W2, G2, C2) :-
+    %% remove fail and add body/other cases for this predicate
+    fail.
---
+++ query/query.pl
@@ -0,0 +1,23 @@
+year_1953_1996_novels(Book) :-
+    %% remove fail and add body/other cases for this predicate
+    fail.
+
+period_1800_1900_novels(Book) :-
+    %% remove fail and add body/other cases for this predicate
+    fail.
+
+lotr_fans(Fan) :-
+    %% remove fail and add body/other cases for this predicate
+    fail.
+
+author_names(Author) :-
+    %% remove fail and add body/other cases for this predicate
+    fail.
+
+fans_names(Fan) :-
+    %% remove fail and add body/other cases for this predicate
+    fail.
+
+mutual_novels(Book) :-
+    %% remove fail and add body/other cases for this predicate
+    fail.
